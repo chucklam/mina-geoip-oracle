807475bec9bd48c2cb7989c09c1c0b1239b18f70
Chuck Lam <939400+chucklam@users.noreply.github.com>
2022-12-13 15:57:45 -0800

    Add IP lookup route and default port

--- index.ts
+++ index.ts
@@ -2,12 +2,16 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const port = process.env.PORT || 3000;
 
 const app: Express = express();
-const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/ip/:ip', (req: Request, res: Response) => {
+  res.send(`You requested geolocation for IP: ${req.params.ip}`);
 });
 
 app.listen(port, () => {
