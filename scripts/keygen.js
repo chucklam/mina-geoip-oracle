0ee71c607fba127af38b0d32602f737da0b4aee9
Chuck Lam <939400+chucklam@users.noreply.github.com>
2022-12-13 16:48:13 -0800

    Add keygen script

---
+++ scripts/keygen.js
@@ -0,0 +1,14 @@
+// Copied from https://github.com/jackryanservia/mina-credit-score-signer/blob/main/scripts/keygen.js
+const { isReady, PrivateKey, shutdown } = require("snarkyjs");
+
+async function generateKeypair() {
+  await isReady;
+  const privateKey = PrivateKey.random();
+  const publicKey = privateKey.toPublicKey();
+  const encodedPrivateKey = privateKey.toBase58();
+  const encodedPublicKey = publicKey.toBase58();
+
+  console.log({ privateKey: encodedPrivateKey, publicKey: encodedPublicKey });
+}
+
+generateKeypair().then(shutdown);
