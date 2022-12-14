1feddee5bb3150a84906a975d77427bdf792bce9
Chuck Lam <939400+chucklam@users.noreply.github.com>
2022-12-13 17:56:04 -0800

    Initialize public/private key before running server

--- index.ts
+++ index.ts
@@ -1,21 +1,32 @@
 import express, { Express, Request, Response } from 'express';
+import { isReady, PrivateKey, PublicKey, Field, Signature } from 'snarkyjs';
 import dotenv from 'dotenv';
-
+import { exit } from 'process';
 dotenv.config();
-const port = process.env.PORT || 3000;
+
+const { PORT, PRIVATE_KEY } = process.env;
+if (!PRIVATE_KEY) {
+  console.error('Private key not specified');
+  exit(1);
+}
+
+const port = PORT || 3000;
 
 const app: Express = express();
+
+// The public/private key of our account
+let privateKey: PrivateKey;
+let publicKey: PublicKey;
 
 const getCountryCode = async (ip: string): Promise<string> => {
   const response = await fetch(`http://ip-api.com/json/${ip}`);
   const json = await response.json();
-  console.log(json);
 
   // Could also get "region", "lat", "lon", "timezone", etc.
   const { countryCode } = json;
 
   return countryCode;
-}
+};
 
 app.get('/ip/:ip', async (req: Request, res: Response) => {
   const { ip } = req.params;
@@ -26,6 +37,11 @@ app.get('/ip/:ip', async (req: Request, res: Response) => {
   res.json({ countryCode });
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+isReady.then(() => {
+  privateKey = PrivateKey.fromBase58(PRIVATE_KEY);
+  publicKey = privateKey.toPublicKey();
+
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  });
 });
